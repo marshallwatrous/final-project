90b8be5feade3ad9f840be7c6223114fd73df77f
marshallwatrous <marshallwatrous95@gmail.com>
2021-09-21 14:48:21 -0700

    Edits server.js

--- server.js
+++ server.js
@@ -7,3 +7,19 @@ const app = express();
 const PORT  4000;
 const rowdyResults = rowdy.begin(app);
 const myController = require("./controller/myController");
+const e = require("express");
+
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use("/final-project", myController);
+
+app.get("/", (req, res) => {
+    res.render("homepage.ejs");
+})
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`The server is running on port ${PORT}`);
+    rowdyResults.print();
+})
