9efaabd3b894c7e8b0e8257db07f858e08087f5d
marshallwatrous <marshallwatrous95@gmail.com>
2021-09-21 16:04:26 -0700

    Edits controller and adds Post.js

--- controller/myController.js
+++ controller/myController.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const db = require("..models/index.js");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+
+    db.Post.find({ user: req.session.currentUser._id }, (err, allPosts) => {
+        if(err) return console.log(err);
+    })
+})
