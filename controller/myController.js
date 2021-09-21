94caa8d6f18ff8aa2c3cf7176e85de7b3c5028d2
marshallwatrous <marshallwatrous95@gmail.com>
2021-09-21 16:40:57 -0700

    Edits controller and adds and edits edit.ejs

--- controller/myController.js
+++ controller/myController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
 
     db.Entry.find({ user: req.session.currentUser._id }, (err, allEntries) => {
-        if(err) return console.log(err);
+        if (err) return console.log(err);
 
         res.render("list.ejs", { allEntries: allEntries })
     })
@@ -25,4 +25,39 @@ router.get("/:entryId", (req, res) => {
         res.render("post.ejs", { oneEntry: foundEntry })
     })
 })
+
+router.post("/", (req, res) => {
+    
+    console.log(req.session);
+
+    console.log("req.body ==>", req.body);
+
+    req.body.user = req.session.currentUser._id;
+
+    db.Entry.create(req.body, (err, createdEntry) => {
+        if (err) return console.log(err);
+
+        console.log(createdEntry);
+
+        res.redirect("/list")
+    });
+})
+
+router.delete("/:entryId", (req, res) => {
+    
+    db.Entry.findByIdAndDelete(req.params.entryId, (err) => {
+        if (err) return console.log(err);
+
+        res.redirect("/list");
+    })
+})
+
+router.get("/:entryId/edit", (req, res) => {
+
+    db.Entry.findById(req.params.entryId, (err, foundEntry) => {
+        if (err) return console.log(err);
+
+        res.render("edit.ejs", { oneEntry: foundEntry });
+    })
+});
 
