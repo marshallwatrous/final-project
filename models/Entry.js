66f3004f0bb4d86223ee0972825a3a57f4751673
marshallwatrous <marshallwatrous95@gmail.com>
2021-09-23 10:14:08 -0700

    Edits Entry.js, home.ejs, and post.ejs

--- models/Entry.js
+++ models/Entry.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const entrySchema = new mongoose.Schema({
     picture: { type: String, required: false },
     name: { type: String, required: false },
+    dateSeen: { type: String, required: false },
     numberSeen: { type: Number, required: false },
     place: { type: String, required: false }
 })
